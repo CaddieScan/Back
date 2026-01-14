6edbc84049500c8cf7ced46d24ac3cfc65c249f8
NonoIceOff <nonocraft28@gmail.com>
2026-01-14 15:52:00 +0100

    ajout d'une route

--- api/routes/product.py
+++ api/routes/product.py
@@ -66,13 +66,32 @@ LOG = logging.getLogger(__name__)
 @router.get("/get_product", response_model=Product)
 def get_product(barcode: int, session: Session = Depends(get_session)):
     try:
-        sql = text("SELECT * FROM produit WHERE code_barre = :barcode")
-        row = session.execute(sql, {"barcode": barcode}).first()
+        sql = text("SELECT * FROM produit ")
+        row = session.execute(sql)
 
         if row is None:
             raise HTTPException(status_code=404, detail="Produit non trouvé")
 
         product = Product(**row._mapping)
+
+        LOG.info(f"Produit trouvé: {product}")
+        return product
+
+    except Exception as e:
+        LOG.error(f"Erreur récupération produit: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.get("/get_product_by_barcode", response_model=Product)
+def get_product(barcode: int, session: Session = Depends(get_session)):
+    try:
+        sql = text("SELECT * FROM produit WHERE code_barre = :barcode")
+        row = session.execute(sql, {"barcode": barcode}).mappings().first()
+
+        if row is None:
+            raise HTTPException(status_code=404, detail="Produit non trouvé")
+
+        product = Product(**row)
 
         LOG.info(f"Produit trouvé: {product}")
         return product
