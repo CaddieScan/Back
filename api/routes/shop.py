2f2f387182f25f8ddcd2992da3c48d721cee2674
Wolphen <enzo7825@gmail.com>
2026-04-29 18:01:08 +0200

    add

--- api/routes/shop.py
+++ api/routes/shop.py
@@ -411,6 +411,29 @@ def set_favorite(body: SetFavoriteShop, session: Session = Depends(get_session))
         LOG.error(f"Erreur ajout favori magasin: {e}")
         raise HTTPException(status_code=500, detail=str(e))
 
+# mettre en favori un magasin
+@router.post("/favorite")
+def set_favorite(body: SetFavoriteShop, session: Session = Depends(get_session)):
+    print("SHOP ID :", body.shop_id)
+    try:
+
+        user_id = 1
+
+        # Insertion dans la table de liaison (ajustez le nom de la table et des colonnes si besoin)
+        sql_insert = text("""
+            INSERT INTO magasin_favori (utilisateur_id, magasin_id) 
+            VALUES (1, 1)
+        """)
+        session.execute(sql_insert, {"utilisateur_id": 1, "magasin_id": body.shop_id})
+        session.commit()
+
+        return {"message": "Magasin ajouté aux favoris avec succès."}
+
+    except Exception as e:
+        session.rollback()
+        LOG.error(f"Erreur ajout favori magasin: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
+
 # trouver les magasins les plus proche selon le GPS
 @router.post("/proximity", response_model=list[Shop])
 def get_proximity_shops(body: GetProximityShops, user_id: int = 1, session: Session = Depends(get_session)):
