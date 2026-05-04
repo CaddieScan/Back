103fe82d5c3e28ae9c01540bb9a034344ff1fad9
TheStarBars <aoges@orange.fr>
2026-05-04 14:47:32 +0200

    ADD: dynamique url pour la base de donnée

--- api/database.py
+++ api/database.py
@@ -1,13 +1,21 @@
+import os
 from sqlmodel import create_engine, Session, SQLModel
 
-POSTGRES_USER = "postgres"
-POSTGRES_PASSWORD = "postgres"
-POSTGRES_DB = "caddiescan"
-POSTGRES_HOST = "localhost"  
-POSTGRES_PORT = 5432
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-postgres_url = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
-engine = create_engine(postgres_url, echo=True)
+if not DATABASE_URL:
+    POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
+    POSTGRES_DB = os.getenv("POSTGRES_DB", "caddiescan")
+    POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+    POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+
+    DATABASE_URL = (
+        f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
+        f"@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    )
+
+engine = create_engine(DATABASE_URL, echo=True)
 
 
 def create_db_and_tables():
