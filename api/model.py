40f0795d8de67935bb043b5edfa6817740393376
TheStarBars <aoges@orange.fr>
2026-05-05 14:05:32 +0200

    CORRECTION: Field

--- api/model.py
+++ api/model.py
@@ -28,8 +28,8 @@ class Produit(SQLModel, table=True):
 
 class Carts(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    user_id : int
-    shop_id : int
-    barcode : int
-    total_price : int
-    creation_time : datetime = Field(default_factory=datetime.now)
+    user_id: int = Field()
+    shop_id: int = Field()
+    barcode: int = Field()
+    total_price: int = Field()
+    creation_time: datetime = Field(default_factory=datetime.now)
